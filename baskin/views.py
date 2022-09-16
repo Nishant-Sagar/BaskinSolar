c39d14f16e9fa70815da4f18402f7730191a4587
Nishant <nishantsagar045@gmail.com>
2022-09-16 23:03:18 +0530

    add form

--- baskin/views.py
+++ baskin/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(requests):
     return render(requests, 'baskin/index.html')
+
+def form(requests):
+    return render(requests, 'baskin/form.html')
