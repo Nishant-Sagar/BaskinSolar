1b5689a431ffad45d2b6db1b045ade16e1830fbf
Nishant <nishantsagar045@gmail.com>
2022-11-05 21:55:09 +0530

    add urls.py

--- baskin/urls.py
+++ baskin/urls.py
@@ -2,6 +2,8 @@ from django.urls import URLPattern, path
 from . import views
 # from 'baskin' import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home, name = 'home'),
@@ -18,3 +20,8 @@ urlpatterns = [
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 #     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
